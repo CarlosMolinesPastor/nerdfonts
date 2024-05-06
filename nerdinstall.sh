db36f7b8c401b6393ffec97694d7f17d09e1f5db
Carlos Molines Pastor <karlinux47@gmail.com>
2024-05-07 00:14:28 +0200

    Update nerdinstall.sh
    Update only the most popular

--- nerdinstall.sh
+++ nerdinstall.sh
@@ -2,51 +2,19 @@
 
 declare -a fonts=(
 	Agave
-	AnonymousPro
-	Arimo
-	AurulentSansMono
-	BigBlueTerminal
-	BitstreamVeraSansMono
 	CascaidaCode
 	CodeNewRoman
-	Cousine
-	DaddyTimeMono
 	DejaVuSansMono
 	DroidSansMono
-	FantasqueSansMono
 	FiraCode
 	FiraMono
-	Go-Mono
-	Gohu
 	Hack
-	Hasklig
-	HeavyData
-	Hermit
-	iA-Writer
-	IBMPlexMono
-	Inconsolate
-	InconsolataGo
-	InconsolataLGC
 	Iosevka
 	JetBrainsMono
-	Lekton
 	LiberationMono
-	Lilex
 	Meslo
-	Monofur
-	Mononoki
 	Monoid
-	MPlus
 	NerdFontsSymbolsOnly
-	Noto
-	OpenDyslexic
-	Overpass
-	ProFont
-	ProggyClean
-	RobotoMono
-	ShareTechMono
-	Terminus
-	Tinos
 	Ubuntu
 	UbuntuMono
 	VictorMono
